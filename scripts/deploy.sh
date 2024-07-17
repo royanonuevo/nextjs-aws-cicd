beaf24e5f52edd4d5a7f388ae5ad4f88d6a17641
Roy Anonuevo <roy.anonuevo@growthops.asia>
2024-07-17 16:07:59 +0800

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ sudo -i bash <<EOF
 echo "***********************************"
 echo "Navigate to the docker folder"
 echo "***********************************"
-cd /root/docker
+cd /home/ubuntu
 
 echo "***********************************"
 echo "Stop the application"
